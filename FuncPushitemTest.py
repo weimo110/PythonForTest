bf76c5c41fb83a74b62db233fbdd561623e34da9
weimo110 <weimo110@gmail.com>
2020-06-09 15:09:04 +0800

    请求脚本

--- FuncPushitemTest.py
+++ FuncPushitemTest.py
@@ -11,32 +11,15 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 # photo
 def main(token):
 
-    payload = "{\"task_id\": \"123456\"," \
-              "\"timestamp\": \"1536991270851\"," \
-              "\"image_base64\": \"" \
-              "\",\"object_list\": [" \
-              "{" \
-                    "\"object_id\": \"71804\"," \
-                    "\"min\": 1," \
-                    "\"max\": 15," \
-                    "\"critical_info\": \"\"}]}"
-
-    # payload = "{\"image_base64\": \"\"," \
-    #           "\"mode\": \"noah-doc\"}"
-
-    # payload = "{\"image_base64\": \"\"}"
+    payload = "{}"
 
     headers = {
     'Content-Type': "application/json",
     'Authorization': 'Bearer ' + token,
     }
 
-    url = "https://10.137.106.120:8343/qcheck2/api/v1/te/detect"
-    # url = "https://10.137.106.120:8343/qcheck/api/v1/ics/detect"
-    # url = "https://10.137.106.120:8343/qcheck/api/v1/ehs/detect"
-    # url = "https://10.137.106.120:8343/qcheck/api/v1/ocr/general-text"
-    # url = "https://10.137.106.120:8343/qcheck/api/v1/ocr/detection-text"
-    # url = "https://10.137.106.120:8343/qcheck/api/v1/ocr/recognition-text"
+    url = "https://"
+
 
     response = requests.request("POST", url, data = payload, headers = headers,verify = False)
 
